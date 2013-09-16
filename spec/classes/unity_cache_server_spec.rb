7969aa95c7485d76f4cd188408199e130177691f
Lars Bilke <lars.bilke@ufz.de>
2013-09-16 16:32:16 +0200

    Specs fixed.

--- spec/classes/unity_cache_server_spec.rb
+++ spec/classes/unity_cache_server_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
 
 describe 'unity_cache_server' do
-	let(:facts) do
-		{
-			:boxen_home => '/opt/boxen',
-			:boxen_user => 'testuser',
-		}
-	end
+  let(:facts) do
+    {
+      :boxen_home => '/opt/boxen',
+      :boxen_user => 'testuser',
+    }
+  end
 
-	it do
-		should include_class('unity_cache_server::config')
+  it do
+    should include_class('unity_cache_server::config')
 
-		should contain_package('CacheServer').with({
-			:source   => 'http://netstorage.unity3d.com/unity/CacheServer-4.2.1.zip',
-			:provider => 'compressed_app'
-			})
+    should contain_package('CacheServer').with({
+      :source   => 'http://netstorage.unity3d.com/unity/CacheServer-4.2.1.zip',
+      :provider => 'compressed_app'
+    })
 
-		should contain_service('unity_cache_server').with(:ensure => 'running')
-  	end
+    should contain_service('unity_cache_server').with(:ensure => 'running')
+  end
 end
